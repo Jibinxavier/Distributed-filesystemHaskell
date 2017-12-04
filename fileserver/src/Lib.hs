dbe7b13da4b8c55b555298ab2e19eff815b7d5d1
Jibin Xavier <xavierj@tcd.ie>
2017-12-04 21:57:26 +0000

    add dependencies

--- fileserver/src/Lib.hs
+++ fileserver/src/Lib.hs
@@ -1,6 +1,54 @@
+
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE DeriveAnyClass       #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE StandaloneDeriving   #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
 module Lib
-    ( someFunc
+    ( startApp
     ) where
+import           Control.Concurrent           (forkIO, threadDelay)
+import           Control.Monad                 
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Except   (ExceptT)
+import           Control.Monad.Trans.Resource
+import           Data.Aeson
+import           Data.Aeson.TH
+import           Data.Bson.Generic
+import qualified Data.ByteString.Lazy         as L
+import qualified Data.List                    as DL
+import           Data.Maybe
+import           Data.Text                    (pack, unpack)
+import           Data.Time.Clock              (UTCTime, getCurrentTime)
+import           Data.Time.Format             (defaultTimeLocale, formatTime)
+import           Database.MongoDB
+import           GHC.Generics
+import           Network.HTTP.Client          (defaultManagerSettings,
+                                               newManager)
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Network.Wai.Logger
+import           Servant
+import qualified Servant.API                  as SC
+import qualified Servant.Client               as SC
+import           System.Environment           (getArgs, getProgName, lookupEnv)
+import           System.Log.Formatter
+import           System.Log.Handler           (setFormatter)
+import           System.Log.Handler.Simple
+import           System.Log.Handler.Syslog
+import           System.Log.Logger
+import           Crypto.BCrypt
+import qualified Data.ByteString.Char8        as BS
+import           FilesystemAPI  
+import           FilesystemAPIServer  
+import           Datatypes 
+import           EncryptionAPI
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
